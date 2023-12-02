922f3a8c98023274bc789db053438cfd8768d3df
Piotr Szatyński <szatynskip@gmail.com>
2023-12-02 15:31:49 +0100

    update ImageGalletyItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,20 +1,30 @@
+import React, { Component } from 'react';
 import css from './ImageGalleryItem.module.css';
+import PropTypes from 'prop-types';
 
-import React from 'react';
+class ImageGalleryItem extends Component {
+  handleClick = () => {
+    this.props.onImageClick(this.props.image);
+  };
 
-const ImageGalleryItem = props => {
-  const { webformatURL, id, tags, largeImageURL, showMod } = props;
+  render() {
+    return (
+      <li className={css.ImageGalleryItem}>
+        <img
+          className={css['ImageGalleryItem-image']}
+          src={this.props.image.webformatURL}
+          alt=""
+          onClick={this.handleClick}
+          loading="lazy"
+        />
+      </li>
+    );
+  }
+}
 
-  return (
-    <li className={css.imageGalleryItem} key={id}>
-      <img
-        className={css.imageGalleryItemImage}
-        src={webformatURL}
-        alt={tags}
-        onClick={()=> showMod(largeImageURL)}
-      />
-    </li>
-  );
+ImageGalleryItem.propTypes = {
+  image: PropTypes.object.isRequired,
+  onImageClick: PropTypes.func.isRequired,
 };
 
 export default ImageGalleryItem;
